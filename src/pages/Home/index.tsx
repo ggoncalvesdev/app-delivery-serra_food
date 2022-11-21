49ee9dda5dff540b42fd7c2f1bdca13e003c023f
clarissavogel <clarissa.vogel@gmail.com>
2022-11-21 17:58:10 -0300

    Comecando a home

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,116 @@
+import React from "react";
+import { Image, Text, TouchableOpacity, View, ScrollView } from "react-native";
+import { styles } from "./style";
+
+import McDonalds from "../../assets/img/mcdonalds.png"
+export const Home = () =>{
+
+    return(
+        <View style={styles.container}>
+
+        <ScrollView>
+            <ScrollView 
+            horizontal 
+            showsHorizontalScrollIndicator={false}
+            style={styles.navbar}>
+               
+                <TouchableOpacity style={styles.buttonNavbar}>
+                    <Text style={styles.textNavbar}>Início</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonNavbar}>
+                    <Text style={styles.textNavbar}>Restaurantes</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonNavbar}>
+                    <Text style={styles.textNavbar}>Mercados</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonNavbar}>
+                    <Text style={styles.textNavbar}>Bebidas</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonNavbar}>
+                    <Text style={styles.textNavbar}>Farmácias</Text>
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.buttonNavbar}>
+                    <Text style={styles.textNavbar}>Pets</Text>
+                </TouchableOpacity>
+
+            </ScrollView>
+
+            <View style={styles.containerLastStore}>
+                <Text style={styles.titleLastStore}>Últimos restaurantes</Text>
+                <ScrollView horizontal
+                showsHorizontalScrollIndicator={false}
+                style={styles.contentLastStore}>
+                    <TouchableOpacity style={styles.lastStore}>
+                        <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                        <Text>Mc Donald's</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.lastStore}>
+                        <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                        <Text>Mc Donald's</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.lastStore}>
+                        <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                        <Text>Mc Donald's</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.lastStore}>
+                        <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                        <Text>Mc Donald's</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.lastStore}>
+                        <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                        <Text>Mc Donald's</Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
+            
+
+            <View style={styles.containerLastStore}>
+                <Text style={styles.titleLastStore}>Favoritos</Text>
+                <ScrollView horizontal
+                showsHorizontalScrollIndicator={false}
+                style={styles.contentLastStore}>
+                    <TouchableOpacity style={styles.lastStore}>
+                        <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                        <Text>Mc Donald's</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.lastStore}>
+                        <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                        <Text>Mc Donald's</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.lastStore}>
+                        <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                        <Text>Mc Donald's</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.lastStore}>
+                        <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                        <Text>Mc Donald's</Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity style={styles.lastStore}>
+                        <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                        <Text>Mc Donald's</Text>
+                    </TouchableOpacity>
+                </ScrollView>
+            </View>
+
+            <View style={styles.containerStores}>
+                <Text style={styles.titleLastStore}>Lojas</Text>
+
+                <TouchableOpacity style={styles.contentStore}>
+                    <Image style={styles.imageLastStore} source={McDonalds}></Image>
+                    <View style={styles.infoStore}>
+                        <Text>Mc Donald's</Text>
+                        <Text>Lanches</Text>
+                        <Text>R$5,00</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
+
+            </ScrollView>
+        </View>
+    )
+}
