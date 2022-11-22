f9c860a0a6b1f31a10814fcbe904183d147153e6
clarissavogel <clarissa.vogel@gmail.com>
2022-11-22 19:14:47 -0300

    feat(refactor)Pedidos

--- src/pages/Pedidos/style.ts
+++ src/pages/Pedidos/style.ts
@@ -2,6 +2,11 @@ import { StyleSheet, Platform } from "react-native";
 
 export const styles = StyleSheet.create({
 
-
+    container:{
+        flex: 1,
+        backgroundColor: 'white',
+        paddingTop: 64,
+        paddingHorizontal: 30
+    },
     
 })
