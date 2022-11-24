c30f8723ddf0be934cc161a9fa063cd7b90d92bc
Pedro Lacerda <111091387+pedrocarius7@users.noreply.github.com>
2022-11-23 22:35:34 -0300

    excluindo CardCozinha

--- src/components/CardProduto/index.tsx
+++ src/components/CardProduto/index.tsx
@@ -1,40 +1,40 @@
 import React, { useContext } from "react";
-import { Text, TouchableOpacityProps, TouchableOpacity, Image } from "react-native";
+import {
+    Text,
+    TouchableOpacityProps,
+    TouchableOpacity,
+    Image,
+} from "react-native";
 import { listaProdutos } from "../../services/api/Request/ProdutoService";
-import { styles } from "./styles";
+import { styles } from "./style";
 
-
-interface CardProdutoProps extends TouchableOpacityProps{
+interface CardProdutoProps extends TouchableOpacityProps {
     produto: listaProdutos;
     setModal: React.Dispatch<React.SetStateAction<boolean>>;
     setIndexSelecionado: React.Dispatch<React.SetStateAction<string>>;
     setPrecoSelecionado?: React.Dispatch<React.SetStateAction<number>>;
-
 }
-export const CardProduto = ({produto, setModal, setPrecoSelecionado, setIndexSelecionado } : CardProdutoProps) =>{
-
+export const CardProduto = ({
+    produto,
+    setModal,
+    setPrecoSelecionado,
+    setIndexSelecionado,
+}: CardProdutoProps) => {
     // const removeProdutoDoCarrinho = useContext(CarrinhoContexto).removeEquipamentoDoCarrinho
-
-    function abreModal () {
+    /*    function abreModal() {
         setIndexSelecionado(produto.index);
-        setPrecoSelecionado && setPrecoSelecionado(produto.preco)
-        setModal (true);
-    }
-
-    return(
+        setPrecoSelecionado && setPrecoSelecionado(produto.preco);
+        setModal(true);
+    } */
+    /* return (
         <TouchableOpacity onPress={abreModal} style={styles.button}>
-            <Text>
-                {produto.nome}
-            </Text>
-            <Text>
-                {produto.preco}
-            </Text>
-            <TouchableOpacity onPress={() => removeProdutoDoCarrinho (produto.index)}>
+            <Text>{produto.nome}</Text>
+            <Text>{produto.preco}</Text>
+            <TouchableOpacity
+                onPress={() => removeProdutoDoCarrinho(produto.index)}
+            >
                 <Text>REMOVER</Text>
             </TouchableOpacity>
-
         </TouchableOpacity>
-    )
-
-
-}
+    ); */
+};
